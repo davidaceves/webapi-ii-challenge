8e3be6c823fccadd36f35593cb7055dee0bd9d64
David <digital0david@gmail.com>
2019-05-11 13:41:36 -0400

    get post/post post

--- data/db-router.js
+++ data/db-router.js
@@ -11,7 +11,47 @@ const router = express.Router();
         } catch(error) {
             console.log(error) 
             res.status(500).json({
-                message: 'Error retrieving posts'
+                error: "The posts information could not be retrieved."
+            })
+        }
+    });
+
+    router.get('/:id', async (req, res) => {
+        
+        try {
+            const hub = await DB.findById(req.params.id);
+            
+            if(hub) {
+                res.status(200).json(hub);
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                });
+        } 
+            } catch(error) {
+                console.log(error);
+                res.status(500)({
+                    message: "The post information could not be retrieved."
+                })
+            }
+    
+    });
+
+    router.post('/', async (req, res) => {
+        const { title, contents } = req.body;
+
+        if (!title && !contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+            return; 
+        }
+        
+        try {
+            const hub = await DB.insert(req.body);
+            res.status(201).json(hub); 
+        } catch(error) {
+            console.log(error);
+            res.status(500)({
+                error: "There was an error while saving the post to the database"
             })
         }
     })
