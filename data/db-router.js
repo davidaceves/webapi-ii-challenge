390327dc5d45c802de51609c0e9dabe0c43ad91c
David <digital0david@gmail.com>
2019-05-11 14:14:18 -0400

    updated post new post

--- data/db-router.js
+++ data/db-router.js
@@ -39,15 +39,15 @@ const router = express.Router();
 
     router.post('/', async (req, res) => {
         const { title, contents } = req.body;
-
-        if (!title && !contents) {
-            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
-            return; 
-        }
         
         try {
-            const hub = await DB.insert(req.body);
-            res.status(201).json(hub); 
+            if (!title || !contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." }); 
+            } else {
+                const hub = await DB.insert(req.body);
+                res.status(201).json(hub); 
+            }
+           
         } catch(error) {
             console.log(error);
             res.status(500)({
